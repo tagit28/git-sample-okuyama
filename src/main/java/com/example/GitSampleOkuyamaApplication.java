6e7f2bc440ec3a03baf8e5cd7e59a78b92edd7b2
tagit28 <takashi.okuyama@rakus-partners.co.jp>
2022-05-10 11:52:56 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleOkuyamaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleOkuyamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleOkuyamaApplication.class, args);
+	}
+
+}
