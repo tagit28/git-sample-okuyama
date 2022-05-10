f93a257750514aef509117596d83cae50b99ceb0
tagit28 <takashi.okuyama@rakus-partners.co.jp>
2022-05-10 14:00:44 +0900

    second commit

---
+++ src/main/java/com/example/domain/Member.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+public class Member {
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+}
